c1a21a04a5fbeacee287534c8bd370653646460d
zaserzafear <naserdoctor@gmail.com>
2020-10-19 09:16:46 +0700

    delete sd card command

--- pro_mini/pro_mini.ino
+++ pro_mini/pro_mini.ino
@@ -1,15 +1,3 @@
-#include <SPI.h>
-#include <SD.h>
-const byte sdCardModule = 4;
-File myFile;
-const char *pathIndexHtml = "index.html";
-const char *pathJqueryMinJs = "js/jquery.min.js";
-const char *pathBootstrapMinCss = "css/bootstrap.min.css";
-const char *pathBootstrapThemeMinCss = "css/bootstrap-theme.min.css";
-const char *pathBootstrapMinJs = "js/bootstrap.min.js";
-const char *pathLoginCsv = "login.csv";
-const char*  pathWebFile[] = {pathIndexHtml, pathJqueryMinJs, pathBootstrapMinCss, pathBootstrapThemeMinCss, pathBootstrapMinJs, pathLoginCsv};
-
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(2, 3); // RX | TX
 String inComing = "";
@@ -42,14 +30,7 @@ void setup() {
 
   Serial.println("Set Pin Mode...");
   setPin();
-
-  Serial.println("Initializing SD card...");
-  initSdCard();
-
-  Serial.println("Check Web File...");
-  checkWebFile();
 }
-
 
 void loop() {
 
@@ -81,16 +62,6 @@ void readSerialData() {
     relayPowerRestart();
   } else if (inComing.equalsIgnoreCase(clearBios)) {
     relayClearBios();
-  } else if (inComing.equalsIgnoreCase(pathIndexHtml)) {
-    readWebFile(pathIndexHtml);
-  } else if (inComing.equalsIgnoreCase(pathJqueryMinJs)) {
-    readWebFile(pathJqueryMinJs);
-  } else if (inComing.equalsIgnoreCase(pathBootstrapMinCss)) {
-    readWebFile(pathBootstrapMinCss);
-  } else if (inComing.equalsIgnoreCase(pathBootstrapThemeMinCss)) {
-    readWebFile(pathBootstrapThemeMinCss);
-  } else if (inComing.equalsIgnoreCase(pathBootstrapMinJs)) {
-    readWebFile(pathBootstrapMinJs);
   }
 }
 
@@ -113,54 +84,4 @@ void relayClearBios() {
   digitalWrite(clearBiosPin, 1);
   delay(intervalClearBiosJumper);
   digitalWrite(clearBiosPin, 0);
-}
-
-void initSdCard() {
-  if (!SD.begin(sdCardModule)) {
-    Serial.println("initialization failed !");
-  } else {
-    Serial.println("initialization done.");
-  }
-}
-
-void checkWebFile() {
-  unsigned int fileWebCount = (sizeof(pathWebFile) / sizeof(pathWebFile[0]));
-  Serial.print("File Web Count = ");
-  Serial.println(fileWebCount);
-  for (unsigned int i = 0; i < fileWebCount; i++) {
-    char *fileWebName = pathWebFile[i];
-    if (SD.exists(fileWebName)) {
-      Serial.print("SUCCESS - Found ");
-      Serial.print(fileWebName);
-      Serial.println(" file.");
-    } else {
-      Serial.print("ERROR - Can't find ");
-      Serial.print(fileWebName);
-      Serial.println(" file.");
-    }
-  }
-}
-
-String readWebFile(String pathFile) {
-  String fileContent = "";
-  myFile = SD.open(pathFile);
-  if (myFile) {
-    while (myFile.available() > 0) {
-      fileContent = myFile.readStringUntil("\r\n");
-    }
-  }
-  myFile.close();
-  return fileContent;
-}
-
-String checkLogin(String username, String password) {
-  String fileContent = "";
-  myFile = SD.open(pathFile);
-  if (myFile) {
-    while (myFile.available() > 0) {
-      fileContent = myFile.readStringUntil("\r\n");
-    }
-  }
-  myFile.close();
-  return fileContent;
 }
