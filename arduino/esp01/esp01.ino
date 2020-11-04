6ecb84827e11e79285ac066b58a56d40e1aae109
zaser zafear <naserdoctor@gmail.com>
2020-11-04 16:17:25 +0700

    use spiffs web for esp-01

--- arduino/esp01/esp01.ino
+++ arduino/esp01/esp01.ino
@@ -1,7 +1,10 @@
+String inComing = "";
+
 #include <ESP8266WiFiMulti.h>
 ESP8266WiFiMulti wifiMulti;
 void checkWiFiConnect() {
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi re connecting...");
     if (wifiMulti.run() == WL_CONNECTED) {
       Serial.println("WiFi connected");
       Serial.println("IP address: ");
@@ -10,98 +13,22 @@ void checkWiFiConnect() {
   }
 }
 
-#include <ESP8266WebServer.h>
-ESP8266WebServer server(80);
-const String linkIndexHtml = "";
-const String linkLoginHtml = "";
-const String linkJqueryMinJs = "";
-const String linkBootstrapMinCss = "";
-const String linkBootstrapMinCssMap = "";
-const String linkBootstrapThemeMinCss = "";
-const String linkBootstrapThemeMinCssMap = "";
-const String linkBootstrapMinJs = "";
-void handleRoot() {
-  if (!is_authentified()) {
-    server.sendHeader("Location", "/login");
-    server.sendHeader("Cache-Control", "no-cache");
-    server.send(301);
-  }
-
-  String result = readWebData(linkIndexHtml);
-  server.send(200, "text/html", result);
-}
-void handleLogin() {
-  String msg;
-  if (server.hasHeader("Cookie")) {
-    Serial.print("Found cookie: ");
-    String cookie = server.header("Cookie");
-    Serial.println(cookie);
-  }
-
-  if (server.hasArg("DISCONNECT")) {
-    server.sendHeader("Location", "/login");
-    server.sendHeader("Cache-Control", "no-cache");
-    server.sendHeader("Set-Cookie", "ESPSESSIONID=0");
-    server.send(301);
-    return;
-  }
-
-  if (server.hasArg("USERNAME") && server.hasArg("PASSWORD")) {
-    if (server.arg("USERNAME") == "admin" &&  server.arg("PASSWORD") == "admin") {
-      server.sendHeader("Location", "/");
-      server.sendHeader("Cache-Control", "no-cache");
-      server.sendHeader("Set-Cookie", "ESPSESSIONID=1");
-      server.send(301);
-      Serial.println("Log in Successful");
-      return;
-    }
-
-    msg = "Wrong username/password! try again.";
-    Serial.println("Log in Failed");
-  }
-
-  String content = "<html><body><form action='/login' method='POST'>To log in, please use : admin/admin<br>";
-  content += "User:<input type='text' name='USERNAME' placeholder='user name'><br>";
-  content += "Password:<input type='password' name='PASSWORD' placeholder='password'><br>";
-  content += "<input type='submit' name='SUBMIT' value='Submit'></form>" + msg + "<br>";
-
-  server.send(200, "text/html", content);
-}
-void handleJqueryMinJs() {
-  String result = readWebData(linkJqueryMinJs);
-  server.send(200, "text/javascript", result);
-}
-void handleBootstrapMinCss() {
-  String result = readWebData(linkBootstrapMinCss);
-  server.send(200, "text/css", result);
-}
-void handleBootstrapMinCssMap() {
-  String result = readWebData(linkBootstrapMinCssMap);
-  server.send(200, "text/css", result);
-}
-void handleBootstrapThemeMinCss() {
-  String result = readWebData(linkBootstrapThemeMinCss);
-  server.send(200, "text/css", result);
-}
-void handleBootstrapThemeMinCssMap() {
-  String result = readWebData(linkBootstrapThemeMinCssMap);
-  server.send(200, "text/css", result);
-}
-void handleBootstrapMinJs() {
-  String result = readWebData(linkBootstrapMinJs);
-  server.send(200, "text/javascript", result);
-}
-
-String inComing = "";
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+AsyncWebServer server(80);
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
   Serial.println("Start Setup.");
 
+  if (!SPIFFS.begin()) {
+    Serial.println("SPIFFS not ready");
+    return;
+  }
+
   WiFi.mode(WIFI_STA);
   wifiMulti.addAP("Note_WIFI", "11111111");
-
   Serial.println("WiFi connecting");
   while (wifiMulti.run() != WL_CONNECTED) {
     delay(1000);
@@ -114,30 +41,45 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", handleRoot);
-  server.on("/login", handleLogin);
-  server.on("/js/jquery.min.js", handleJqueryMinJs);
-  server.on("/css/bootstrap.min.css", handleBootstrapMinCss);
-  server.on("/css/bootstrap.min.css.map", handleBootstrapMinCssMap);
-  server.on("/css/bootstrap-theme.min.css", handleBootstrapThemeMinCss);
-  server.on("/css/bootstrap-theme.min.css.map", handleBootstrapThemeMinCssMap);
-  server.on("/js/bootstrap.min.js", handleBootstrapMinJs);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+  server.on("/login", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/login.html", "text/html");
+  });
+  server.on("/js/jquery.min.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/js/jquery.min.js", "text/javascript");
+  });
+  server.on("/css/bootstrap.min.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/css/bootstrap.min.css", "text/css");
+  });
+  server.on("/css/bootstrap-theme.min.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/css/bootstrap-theme.min.css", "text/css");
+  });
+  server.on("/js/bootstrap.min.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/js/bootstrap.min.js", "text/javascript");
+  });
+  server.on("/relay", HTTP_POST, [](AsyncWebServerRequest * request) {
+    String relayParamName = "relay";
+    String relayParamValue = "";
+    if (request->hasParam(relayParamName)) {
+      relayParamValue = request->getParam("relay")->value();
+      Serial.println(relayParamValue);
+    }
+    request->send(200, "text/plain", "param name : " + relayParamName + " // param value : " + relayParamValue);
+  });
+  server.onNotFound([](AsyncWebServerRequest * request) {
+    request->send(404, "text/html", "<H1>HTTP ERROR 404</H1>");
+  });
 
-  server.on("/relay", handleRelay);
-  server.onNotFound(handleNotFound);
-  const char * headerkeys[] = {"User-Agent", "Cookie"} ;
-  size_t headerkeyssize = sizeof(headerkeys) / sizeof(char*);
-  server.collectHeaders(headerkeys, headerkeyssize);
   server.begin();
-  Serial.println("HTTP server started");
-  readWebData();
+  Serial.println("Server Begin complete!!!");
+  Serial.println();
 }
 
 void loop() {
-  //  checkWiFiConnect();
-  server.handleClient();
-
   readSerialData();
+  checkWiFiConnect();
 }
 
 void readSerialData() {
@@ -146,42 +88,6 @@ void readSerialData() {
     if (inComing != "") {
       inComing.trim();
       Serial.println(inComing);
-
-      if (inComing == "readWebData") {
-        String webData = readWebData();
-      }
     }
   }
-}
-
-void handleRelay() {
-  if (!is_authentified()) {
-    server.sendHeader("Location", "/login");
-    server.sendHeader("Cache-Control", "no-cache");
-    server.send(301);
-    return;
-  }
-
-  if (server.hasArg("relay")) {
-    Serial.println(server.arg("relay"));
-  }
-}
-
-void handleNotFound() {
-  server.send(404, "text/html", "<h1>404 error: File not found</h1>");
-}
-
-bool is_authentified() {
-  Serial.println("Enter is_authentified");
-  if (server.hasHeader("Cookie")) {
-    Serial.print("Found cookie: ");
-    String cookie = server.header("Cookie");
-    Serial.println(cookie);
-    if (cookie.indexOf("ESPSESSIONID=1") != -1) {
-      Serial.println("Authentification Successful");
-      return true;
-    }
-  }
-  Serial.println("Authentification Failed");
-  return false;
 }
--- arduino/pro_mini/pro_mini.ino
+++ arduino/pro_mini/pro_mini.ino
@@ -1,80 +1,12 @@
-#include <SPI.h>
-#include <SD.h>
-void initSdCard() {
-  const byte sdCardChipSelect = 4;
-  Sd2Card card;
-  if (!card.init(SPI_HALF_SPEED, sdCardChipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    while (1);
-  } else {
-    Serial.println("Wiring is correct and a card is present.");
-  }
-  Serial.print("Card type : ");
-  switch (card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-  SdVolume volume;
-  if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    while (1);
-  }
-  Serial.print("Volume type is : FAT");
-  Serial.println(volume.fatType(), DEC);
-  uint32_t volumesize;
-  volumesize = volume.blocksPerCluster();
-  volumesize *= volume.clusterCount();
-  volumesize /= 2;
-  Serial.print("Volume size (Kb) : ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mb) : ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Gb) : ");
-  Serial.println((float)volumesize / 1024.0);
-}
-void readFromSdCard(String filePath = "test.txt") {
-  File myFile;
-  myFile = SD.open(filePath);
-  if (myFile) {
-    Serial.print(filePath);
-    Serial.println(" : ");
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    myFile.close();
-  } else {
-    Serial.print("error opening ");
-    Serial.println(filePath);
-  }
-}
-
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(2, 3); // RX | TX
+SoftwareSerial mySerial(3, 2); // RX | TX
 String inComing = "";
-
-unsigned long runTime = 0;
-unsigned long previousTime = 0;
-unsigned long timeInterval = 1000;
-
 const byte powerPin = 14; //A0
-const String powerOn = "power";
+const char* powerOn = "power";
 const byte restartPin = 15; //A1
-const String powerRestart = "restart";
+const char* powerRestart = "restart";
 const byte clearBiosPin = 16; //A2
-const String clearBios = "clear";
+const char* clearBios = "clear";
 const unsigned long intervalPowerButton = 255;
 const unsigned long intervalClearBiosJumper = 5000;
 
@@ -82,17 +14,14 @@ void setPin() {
   pinMode(powerPin, OUTPUT);
   pinMode(restartPin, OUTPUT);
   pinMode(clearBiosPin, OUTPUT);
+  pinMode(17, OUTPUT);
 }
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Serial.begin(115200);");
+  Serial.begin(9600);
+  Serial.println("Serial.begin(9600);");
   mySerial.begin(9600);
   Serial.println("mySerial.begin(9600)");
-  Serial.println();
-  Serial.println("Initializing SD card...");
-  initSdCard();
-  readFromSdCard();
   Serial.println();
   Serial.println("Set Pin Mode...");
   setPin();
@@ -100,7 +29,6 @@ void setup() {
 }
 
 void loop() {
-
   while (Serial.available() > 0) {
     inComing = Serial.readStringUntil('\n');
     if (inComing != "") {
@@ -133,22 +61,28 @@ void readSerialData() {
 }
 
 void relayPowerOn() {
-  Serial.println("Power.");
+  Serial.println("relayPowerOn");
   digitalWrite(powerPin, 1);
+  digitalWrite(17, 1);
   delay(intervalPowerButton);
+  digitalWrite(17, 0);
   digitalWrite(powerPin, 0);
 }
 
 void relayPowerRestart() {
-  Serial.println("Restart.");
+  Serial.println("relayPowerRestart");
   digitalWrite(restartPin, 1);
+  digitalWrite(17, 1);
   delay(intervalPowerButton);
+  digitalWrite(17, 0);
   digitalWrite(restartPin, 0);
 }
 
 void relayClearBios() {
-  Serial.println("Clear BIOS.");
+  Serial.println("relayPowerRestart");
   digitalWrite(clearBiosPin, 1);
+  digitalWrite(17, 1);
   delay(intervalClearBiosJumper);
+  digitalWrite(17, 0);
   digitalWrite(clearBiosPin, 0);
 }
